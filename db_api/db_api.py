aa378c520b59abbf7f61b8d7fa79f6724277813a
DanikNik <nikolsky.dan@gmail.com>
2018-02-15 01:37:02 +0300

    tyyysa

--- db_api/db_api.py
+++ db_api/db_api.py
@@ -2,10 +2,10 @@ import sqlite3
 import json
 DB_PATH = '../database/state.db'
 
-def gen_json(data, table):
-    for person in data:
-        with open("{}_{}.json".format(str(person['id']), table), "w") as user_json:
-            json.dump(person, user_json)
+# def gen_json(data, table):
+    # for person in data:
+        # with open("{}_{}.json".format(str(person['id']), table), "w") as user_json:
+            # json.dump(person, user_json)
 
 class database:
     def __init__(self, db_path):
@@ -19,22 +19,22 @@ class database:
         self.cursor.execute('INSERT INTO state ({}) VALUES (\"{}\")'.format(*query))
         self.connection.commit()
 
-    def get_user_by_id(self, id):
-        self.cursor.execute("SELECT * from state WHERE id = ?", id)
-        data = [dict(db.cursor.fetchone())]
-        gen_json(data, "state")
+    def get_user_by_id(self, usr_id):
+        self.cursor.execute("SELECT * from state WHERE id = \'{}\'".format(usr_id))
+        data = [dict(self.cursor.fetchone())]
+        # gen_json(data, "state")
         return(data)
 
     def get_by_value(self, table, val_name, value):
         self.cursor.execute("SELECT * from {0} where {1} = \'{2}\'".format(table, val_name, value))
-        data = [dict(person) for person in db.cursor.fetchall()]
-        gen_json(data, table)
+        data = [dict(person) for person in self.cursor.fetchall()]
+        # gen_json(data, table)
         return(data)
 
     def get_scoring_history(self, id):
         self.cursor.execute("SELECT * from scoring where id = ?", id)
-        data = [dict(person) for person in db.cursor.fetchall()]
-        gen_json(data, scoring)
+        data = [dict(person) for person in self.cursor.fetchall()]
+        # gen_json(data, scoring)
         return(data)
 
 if __name__ == '__main__':
