830347c72f40621a40070e4f35849fe6d9ae099e
DanikNik <nikolsky.dan@gmail.com>
2018-02-14 18:38:28 +0300

    db_api polishing (shit polishing)

--- db_api/main.py
+++ db_api/main.py
@@ -2,9 +2,10 @@ import sqlite3
 import json
 DB_PATH = '../database/state.db'
 
-def gen_json_persdata(data):
-    with open("{}.json".format(str(person['id'])), "w") as user_json:
-        json.dump(person, user_json)
+def gen_json(data, table):
+    for person in data:
+        with open("{}_{}.json".format(str(person['id']), table), "w") as user_json:
+            json.dump(person, user_json)
 
 class database:
     def __init__(self, db_path):
@@ -21,19 +22,19 @@ class database:
     def get_user_by_id(self, id):
         db.cursor.execute("SELECT * from state WHERE id = ?", id)
         data = [dict(db.cursor.fetchone())]
-        gen_json_persdata(data)
+        gen_json(data, state)
         return(data)
 
-    def get_user_by_value(self, table, val_name, value):
+    def get_by_value(self, table, val_name, value):
         db.cursor.execute("SELECT * from {0} where {1} = \'{2}\'".format(table, val_name, value))
         data = [dict(person) for person in db.cursor.fetchall()]
-        gen_json_persdata(data)
+        gen_json(data, table)
         return data
 
     def get_scoring_history(self, id):
         db.cursor.execute("SELECT * from scoring where id = ?", id)
         data = [dict(person) for person in db.cursor.fetchall()]
-        gen_json_persdata(data)
+        gen_json(data, scoring)
         return(data)
 
     def get_all_data(self, id):
@@ -42,8 +43,5 @@ class database:
 
 if __name__ == '__main__':
     db = database(DB_PATH)
-    id = str(input())
-    
-    # with open("user_info.json") as user_data:
-        # db.register_user(user_data)
-    print(db.get_all_data(id))
+    with open('user_info.json') as info:
+        db.register_user(info)
