1aae9ebdd2e059748c68cca7d2f31198af238f2e
DanikNik <nikolsky.dan@gmail.com>
2018-02-15 12:34:42 +0300

    api to web json  generator

--- interface.py
+++ interface.py
@@ -3,12 +3,9 @@ sys.path.append('./db_api')
 from db_api import database
 import matching_alg
 
-def recieve_id():
-    return("0dc1f1f98869ea423b313f499c159c5b04d889ac")
 
-def main():
+def main(mentee_id):
     db = database("./database/state.db")
-    mentee_id = recieve_id()
     mentee_data = db.get_user_by_id(mentee_id)
     potential_mentors = db.get_by_value("state", "department", mentee_data["department"])
     data = matching_alg.find_mentors(mentee_data, potential_mentors, 5)
@@ -17,5 +14,7 @@ def main():
         mentor_stat.update({mentor: db.generate_out_json(mentor, data[mentor])})
     print(mentor_stat)
     return mentor_stat
+
+
 if __name__ == '__main__':
         main()
