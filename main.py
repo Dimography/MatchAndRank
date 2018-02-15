9b7f7647133aa863c389ef6a72f89bf415a1f1e6
DanikNik <nikolsky.dan@gmail.com>
2018-02-15 11:04:17 +0300

    improved api interface, filled db with state information, minor bug fix

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from db_api.db_api import database
 import match_func
 
 def recieve_id():
-    return("c2ccf9a4fcb0387a702af6abea8592c5e37ca7cf")
+    return("b047741e5ddfc5e866847ae16e04dc1966502d79")
 
-if __name__ == '__main__':
+def main():
     db = database("./database/state.db")
     mentee_id = recieve_id()
     mentee_data = db.get_user_by_id('c2ccf9a4fcb0387a702af6abea8592c5e37ca7cf')
     potential_mentors = db.get_by_value("state", "department", mentee_data["department"])
     data = match_func.find_mentors(mentee_data, potential_mentors, 5)
     print(data)
+    return(data)
+
+if __name__ == '__main__':
+        main()
